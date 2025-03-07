d1ff200f75aada0efbc547e0bfec4a3331a3553a
n00210357 <99271838+n00210357@users.noreply.github.com>
2025-03-07 11:27:38 +0000

    remover reply

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ app.use((req, res, next) =>
 app.use('/api/users', require('./routes/users.js'));
 app.use('/api/puzzles', require('./routes/puzzles.js'));
 app.use('/api/comments', require('./routes/comments.js'));
-app.use('/api/replies', require('./routes/replies.js'));
 app.use('/api/messages', require('./routes/messages.js'));
 app.use('/api/bugs', require('./routes/bugs.js'));
 //app.use('/api/mines', require('./routes/mines.js'));
